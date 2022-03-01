cbad0adcb6bf234b3cbfe2366802431c315df792
Digvijay <joysingh1444@gmail.com>
2022-03-01 14:20:41 +0530

    Spy Test

---
+++ JS/spy.js
@@ -0,0 +1,13 @@
+var a = prompt("Enter your First Name");
+var b = prompt("Enter your last name");
+var h = prompt("Enter Your Height");
+var ch = prompt("Enter your Nickname");
+var i = ch.length;
+var j = 0;
+if (a[j] == b[j] && h >= 170 && ch[i - 1] === "y") {
+    console.log("Great !!! You have passed the SPY Test !!");
+    alert("Welcome Agent Digvijay. The update is some pakistani militants are hiding in Leh!!!! ");
+}
+else {
+    console.log("Sorry There is nothing to show ...")
+}
